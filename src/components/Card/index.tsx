7bdbd59b4955d3cb0a93fa2cde5255b7181ab0b2
Oliver Aparicio Martinez <olivermtz35@gmail.com>
2024-06-16 20:46:36 -0600

    FTR: ajuste

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,4 @@
-import { useNavigate } from 'react-router-dom';
 import './card.css';
-
-import Button from '../Button';
 
 interface Props {
 	index: number;
@@ -11,14 +8,12 @@ interface Props {
 	image: string;
 }
 const Card = ({ index, title, colorTitle, description, image }: Props) => {
-	const navigate = useNavigate();
 	return (
 		<div className={`cardImage h-screen bg-cover ${index % 2 === 0 ? 'cardImage--custom' : ''}`} style={{ backgroundImage: `url(${image})` }}>
 			<div>
 				<h5 className={`cardImage__title font-sans text-${colorTitle} font-bold text-2xl mb-2 text-center`}>{title}</h5>
 				<p className="cardImage__text font-serif">{description}</p>
 				<div className="cardImage__button">
-					<Button text="Ver más" type="primary" onClick={() => navigate('/actividades')} />
 				</div>
 			</div>
 		</div>
